acda6bdae074a1f24080d6e96302bf8b7dc3ccf0
Hw2 <Kucherenko.vv@mail.ru>
2022-12-29 22:39:28 +0800

    First commit spring-maven

---
+++ src/main/java/com/recipiesbook/RecipiesBookApplication.java
@@ -0,0 +1,13 @@
+package com.recipiesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipiesBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recipiesbook/RecipiesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipiesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiesBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
