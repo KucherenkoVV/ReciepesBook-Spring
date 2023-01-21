3fe6b1da239f4d925125fd6b67fc8100e25c48bd
Hw2 <Kucherenko.vv@mail.ru>
2023-01-21 23:02:58 +0800

    RecipesBook spring-maven LIB

--- src/main/java/com/recipiesbook/model/Recipe.java
+++ src/main/java/com/recipiesbook/model/Recipe.java
@@ -2,16 +2,25 @@ package com.recipiesbook.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Recipe {
+    @NotBlank(message = "Name is empty")
     private String nameRecipe;
+    @Positive
     private int minutes;
+    @NotEmpty
     private List <Ingredients> ingredientsList;
+    @NotEmpty
     private List<String> steps;
 
 }
